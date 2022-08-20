db2dccb7851588295d0dca70c631466dbd5f4d61
DaisyOdongo <daisy.odongo@student.moringaschool.com>
2022-08-20 13:24:52 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(rollerSkate= "roller-skate"){
+     return `This Saturday, I want to ${rollerSkate}!`
+    
+}
+
+function mondayWork(goToTheOffice = 'go to the office'){
+    return `This Monday, I will ${goToTheOffice}.`
+   
+}
+function wrapAdjective(flair = '*') {
+   return function (name, adverb = "special") {
+     return (`You are ${flair}${name}${flair}!`)
+   };
+   }
